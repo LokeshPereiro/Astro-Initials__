b3b432c7cd19ab66c7d0e0082bcdab418fffa617
Lokesh Pereiro <lokeshpereiromf@gmail.com>
2024-04-25 00:05:32 +0200

    i zustand for  global state

--- spotify-clone-midudev/src/components/Player.jsx
+++ spotify-clone-midudev/src/components/Player.jsx
@@ -1,4 +1,31 @@
 import { useEffect, useRef, useState } from "react"
+import { usePlayerStore } from "../store/playerStore"
+
+
+const CurrentSong = ({ image, title, artists }) => {
+  return (
+    <div
+      className={`
+        flex items-center gap-5 relative
+        overflow-hidden
+      `}>
+        <picture className="w-16 h-16 bg-zinc-800 rounded-md shadow-lg overflow-hidden">
+          <img src={image} alt={title} />
+        </picture>
+
+        <div className="flex flex-col">
+          <h3 className="font-semibold text-sm block">
+            {title}
+          </h3>
+          <span className="text-xs opacity-80">
+            {artists?.join(', ')}
+          </span>
+        </div>
+
+    </div>
+  )
+}
+
 
 export const Pause = ({ className }) => (
   <svg className={className} role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
@@ -9,48 +36,48 @@ export const Play = ({ className }) => (
 )
 
 export const Player = () => {
-//   const { currentMusic, isPlaying, setIsPlaying, volume } = usePlayerStore(state => state)
-const [isPlaying, setIsPlating] = useState(false)
+  const { currentMusic, isPlaying, setIsPlaying, volume } = usePlayerStore(state => state)
+
   const audioRef = useRef()
 
-//   useEffect(() => {
-//     isPlaying
-//       ? audioRef.current.play()
-//       : audioRef.current.pause()
-//   }, [isPlaying])
+  useEffect(() => {
+    isPlaying
+      ? audioRef.current.play()
+      : audioRef.current.pause()
+  }, [isPlaying])
 
 //   useEffect(() => {
 //     audioRef.current.volume = volume
 //   }, [volume])
 
-//   useEffect(() => {
-//     const { song, playlist, songs } = currentMusic
-//     if (song) {
-//       const src = `/music/${playlist?.id}/0${song.id}.mp3`
-//       audioRef.current.src = src
-//       audioRef.current.volume = volume
-//       audioRef.current.play()
-//     }
-//   }, [currentMusic])
-
-//   const handleClick = () => {
-//     setIsPlaying(!isPlaying)
-//   }
-const handleClick = () => {
-    if(isPlaying){
-         audioRef.current.pause()
-    } else{
-        audioRef.current.src = `/music/1/01.mp3`
-        audioRef.current.play()
+  useEffect(() => {
+    const { song, playlist, songs } = currentMusic
+    if (song) {
+      const src = `/music/${playlist?.id}/0${song.id}.mp3`
+      audioRef.current.src = src
+      audioRef.current.volume = volume
+      audioRef.current.play()
     }
-    setIsPlating(!isPlaying)
-}
+  }, [currentMusic])
+
+  const handleClick = () => {
+    setIsPlaying(!isPlaying)
+  }
+
+// const handleClick = () => {
+//     if(isPlaying){
+//          audioRef.current.pause()
+//     } else{
+//         audioRef.current.src = `/music/1/01.mp3`
+//         audioRef.current.play()
+//     }
+//     setIsPlaying(!isPlaying)
+// }
 
   return (
     <div className="flex flex-row justify-between w-full px-1 z-50">
       <div className="w-[200px]">
-        {/* <CurrentSong {...currentMusic.song} /> */}
-        Current Song..
+        <CurrentSong {...currentMusic.song} />
       </div>
 
       <div className="grid place-content-center gap-4 flex-1">
